dd4896ccc62791c9654b7e6c908a5f69ad33b886
federicot2 <87830686+federicot2@users.noreply.github.com>
2024-06-07 14:20:19 -0500

    fix-../

--- public/js/arbolPanama.js
+++ public/js/arbolPanama.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { XRButton } from 'three/examples/jsm/webxr/XRButton.js';
-import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from '../three';
+import { OrbitControls } from '../three/examples/jsm/controls/OrbitControls.js';
+import { XRButton } from '../three/examples/jsm/webxr/XRButton.js';
+import { XRControllerModelFactory } from '../three/examples/jsm/webxr/XRControllerModelFactory.js';
+import { GLTFLoader } from '../three/examples/jsm/loaders/GLTFLoader.js';
 
 let container;
 let camera, scene, renderer;
